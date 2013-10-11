716c51527aa5e53081a70afd3f912b8ba659f14c
Taylor Otwell <taylorotwell@gmail.com>
2013-10-11 09:14:36 -0500

    first

---
+++ src/Snappy/Apps/ServiceName/App.php
@@ -0,0 +1,49 @@
+<?php namespace Snappy\Apps\ServiceName;
+
+use Snappy\Apps\App as BaseApp;
+
+class App extends BaseApp {
+
+	/**
+	 * The name of the application.
+	 *
+	 * @var string
+	 */
+	public $name = 'Application Name';
+
+	/**
+	 * The application description.
+	 *
+	 * @var string
+	 */
+	public $description = 'Application Description';
+
+	/**
+	 * The application's icon filename.
+	 *
+	 * @var string
+	 */
+	public $icon = 'icon.png';
+
+	/**
+	 * The application author name.
+	 *
+	 * @var string
+	 */
+	public $author = 'Your Name';
+
+	/**
+	 * The application author e-mail.
+	 *
+	 * @var string
+	 */
+	public $email = 'Your Email';
+
+	/**
+	 * The settings required by the application.
+	 *
+	 * @var array
+	 */
+	public $settings = array();
+
+}
